617cf725ed159789f630ce60271e8fcf7c323d74
Daniel 'Akathorn' Espino <akathorn@gmail.com>
2014-05-04 16:52:38 +0200

    continues the implementation of typechecking

--- src/main/scala/elfcala/Signature.scala
+++ src/main/scala/elfcala/Signature.scala
@@ -4,78 +4,84 @@ import scala.language.implicitConversions
 import LogicalFramework._
 
 abstract class Binding
-case class FamilyBinding(d: Family.Const, k: Kind)   extends Binding
-case class ObjectBinding(c: Object.Const, a: Family) extends Binding
-case class VariableBinding(x: Object.Var, a: Family) extends Binding
+abstract class SignatureBinding
+abstract class ContextBinding
+case class FamilyBinding(d: Constant, k: Kind)   extends SignatureBinding
+case class ObjectBinding(c: Constant, a: Family) extends SignatureBinding
+case class VariableBinding(x: Variable, a: Family) extends ContextBinding
 
 trait Signature {
-  var bindings: List[Binding] = Nil
+  var bindings: List[SignatureBinding] = Nil
   var familyConstants: Set[Symbol] = Set.empty
   var objectConstants: Set[Symbol] = Set.empty
 
-  def |-(b: Binding) = b match {
-    case FamilyBinding(Family.Const(Constant(d)), _) =>
+  def |-(b: SignatureBinding) = b match {
+    case FamilyBinding(Constant(d), _) =>
       familyConstants = familyConstants + d
       bindings = bindings :+ b
       b
-    case ObjectBinding(Object.Const(Constant(c)), _) =>
+    case ObjectBinding(Constant(c), _) =>
       objectConstants = objectConstants + c
       bindings = bindings :+ b
       b
   }
 
   def bindFamily(s: Symbol)(k: Kind) =
-    |- (FamilyBinding(Family.Const(Constant(s)), k))
+    |- (FamilyBinding(Constant(s), k))
   def bindObject(s: Symbol)(a: Family) =
-    |- (ObjectBinding(Object.Const(Constant(s)), a))
+    |- (ObjectBinding(Constant(s), a))
+
+
+  // TODO:
+  // implicit def symbolToObjectVariable(s: Symbol): Object = Object.Var(Variable(s))
+  // implicit def symbolToObjectConstant(s: Symbol): Object = Object.Const(Constant(s))
+  implicit def symbolToFamilyConstant(s: Symbol): Family = Family.Const(Constant(s))
 
 
   // Syntax sugaring
   // TODO: it's become huge, should it be in another class?
+
+  // Binders
   case class SymbolBinder(s: Symbol) {
     def :>(k: Kind)   = bindFamily(s)(k)
     def :>(a: Family) = bindObject(s)(a)
-    def :>(o: Symbol) = bindObject(s)(Family.Const(Constant(o)))
 
-
-    def ->:(o: Symbol) = Family.Pi(Variable('x),
-                                   Family.Const(Constant(o)),
-                                   Family.Const(Constant(s)))
-    def ->:(o: Family) = Family.Pi(Variable('x),
-                                   o,
-                                   Family.Const(Constant(s)))
+    def ->:(o: Family) = Family.Pi(Variable('x), o, s)
 
     def apply(o: Symbol): Application = Application(s, Object.Var(Variable(o)))
+    // TODO: we need context information to decide if the second argument of
+    // the application is a variable or a constant
+
     def apply(o: Object): Application = Application(s, o)
+    def apply(o: Application): Application =
+      Application(s, applicationToObject(o))
   }
 
-  case class Application(n: Symbol, m: Object) {
-    def ->:(o: Family) = o ->: FamilyBinder(applicationToFamily(this))
-
-    def apply(o: Symbol) = applicationToFamily(this).apply(o)
-    def apply(o: Object) = applicationToFamily(this).apply(o)
-  }
+  // case class ObjectBinder(t: Object) {
+  //   def apply(o: Symbol): Object = Object.App(t, Object.Var(Variable(o)))
+  //   def apply(o: Object): Object = Object.App(t, o)
+  // }
 
   case class FamilyBinder(a: Family) {
-    def ->:(s: Symbol) = Family.Pi(Variable('x),
-                                   Family.Const(Constant(s)),
-                                   a)
-    def ->:(o: Family) = Family.Pi(Variable('x),
-                                   o,
-                                   a)
+    def ->:(o: Family) = Family.Pi(Variable('x), o, a)
 
     def apply(o: Symbol): Family = Family.App(a, Object.Var(Variable(o)))
+                                    // TODO: it could also be constant...
     def apply(o: Object): Family = Family.App(a, o)
   }
   case class KindBinder(k: Kind) {
-    def ->:(s: Symbol) = Kind.Pi(Variable('x),
-                                 Family.Const(Constant(s)),
-                                 k)
     def ->:(a: Family) = Kind.Pi(Variable('x),
                                  a,
                                  k)
   }
 
+  implicit def symbolToBinder(s: Symbol): SymbolBinder = SymbolBinder(s)
+  implicit def familyToBinder(a: Family): FamilyBinder = FamilyBinder(a)
+//  implicit def objectToBinder(o: Object): ObjectBinder = ObjectBinder(o)
+  implicit def kindToBinder(k: Kind):     KindBinder   = KindBinder(k)
+
+
+  // Pi sugar
   case class PiSugar(bindings: List[Pair[Symbol, Symbol]]) {
     def /(b: Family): Family = {
       def recBind(bindings: List[Pair[Symbol, Symbol]]): Family = bindings match {
@@ -83,19 +89,20 @@ trait Signature {
           b
         case binding :: rest =>
           Family.Pi(Variable(binding._1),
-                    Family.Const(Constant(binding._2)),
+                    binding._2,
                     recBind(rest))
       }
       recBind(bindings)
     }
 
+    // TODO: eliminate code repetition
     def /(k: Kind): Kind = {
       def recBind(bindings: List[Pair[Symbol, Symbol]]): Kind = bindings match {
         case Nil =>
           k
         case binding :: rest =>
           Kind.Pi(Variable(binding._1),
-                  Family.Const(Constant(binding._2)),
+                  binding._2,
                   recBind(rest))
       }
       recBind(bindings)
@@ -109,12 +116,18 @@ trait Signature {
   // TODO: bind Pair[Symbol, Family]
 
 
-  implicit def symbolToBinder(s: Symbol): SymbolBinder = SymbolBinder(s)
-  implicit def familyToBinder(a: Family): FamilyBinder = FamilyBinder(a)
-  implicit def kindToBinder(k: Kind):     KindBinder   = KindBinder(k)
+  // Application sugar
+  case class Application(n: Symbol, m: Object) {
+    def ->:(o: Family) = o ->: FamilyBinder(applicationToFamily(this))
+
+    def apply(s: Symbol): Family = applicationToFamily(this).apply(s)
+    def apply(o: Object): Family = applicationToFamily(this).apply(o)
+  }
+
+
   implicit def applicationToFamily(a: Application): Family = {
     val Application(n, m) = a
-    Family.App(Family.Const(Constant(n)), m)
+    Family.App(n, m)
   }
   implicit def applicationToObject(a: Application): Object = {
     val Application(n, m) = a
